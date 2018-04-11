0eff3095fbc013abf7a55fc0acbf33672d687346
Hokid <mr.hokid@gmail.com>
2018-04-11 19:54:42 +0700

    fix(service-user): lib name

--- service-user/webpack.config.js
+++ service-user/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './commonjs'),
     filename: 'index.js',
-    library: 'ItemsLoadGenerator',
+    library: 'WebappServiceUser',
     libraryTarget: 'commonjs2'
   },
   externals: [
